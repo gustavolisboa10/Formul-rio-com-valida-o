6beafb80810e017bc57980080c88e1ca61db1244
Gustavo Lisboa <gustavolisboadeveloper@gmail.com>
2023-07-03 10:11:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//DOM Manipulation
+const form = document.querySelector("form")
+const nome = document.querySelector("#nome")
+const email = document.querySelector("#email")
+const assunto = document.querySelector("#assunto")
+const mensagem = document.querySelector("#mensagem")
+const erroMessages = document.querySelectorAll(".erro-message")
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+    resetError()
+    validateInputs() // invocação da função
+    
+})
+
+function setError(input, errorMessage){
+    const errorMenssageElement = input.nextElementSibling
+    errorMenssageElement.textContent = errorMessage
+    input.parentElement.classList.add("error")
+}
+
+function resetError(){
+    erroMessages.forEach((msg) =>{
+    msg.textContent = ""
+})
+    nome.parentElement.classList.remove('error')
+     email.parentElement.classList.remove('error')
+     assunto.parentElement.classList.remove('error')
+     mensagem.parentElement.classList.remove('error')
+}
+
+function validateInputs(){
+    const nomeValue = nome.value.trim()  // metodo trim é para não vim espaço em branco do input
+    const emailValue = email.value.trim()
+    const assuntoValue = assunto.value.trim()
+    const mensagemValue = mensagem.value.trim()
+    if(nomeValue === ''){
+        setError(nome, 'nome não pode ficar em branco')
+    }
+    if(emailValue === ''){
+        setError(email, 'e-mail não pode ficar em branco')
+    }else if(!isValidEmail(emailValue)){
+        setError(email,"e-mail inválido")
+    }
+    if(assuntoValue === ''){
+        setError(assunto, 'assunto não pode ficar em branco')
+    }
+    if(mensagemValue === ''){
+        setError(mensagem, 'mensagem não pode ficar em branco')
+    }
+
+}
+
+function isValidEmail(email){
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)  // regex para validação do e-mail
+}
